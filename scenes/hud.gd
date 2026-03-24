7c5c4a8cb9dcd675b706c1f68b9f906e767cacaa
Jason McCoy <jason@virtualcorners.com>
2026-03-23 20:57:21 -0400

    Add new start screen

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,23 +1,30 @@
 extends CanvasLayer
 
 # Notifies 'Main' node that the button has been pressed
-signal start_game
+# signal start_game
 
 func show_message(text):
-	$Message.text = text
-	$Message.show()
+	print(text)
+	$MessageContainer/Message.text = text
+	$MessageContainer/Message.show()
 	$MessageTimer.start()
-
-func show_game_over():
-	show_message("Game Over")
-	# Wait until the MessageTimer has counted down.
+	
 	await $MessageTimer.timeout
 	
-	$Message.text = "Imminent Meltdown!"
-	$Message.show()
+	$MessageContainer/Message.hide()
+	
+
+func show_game_over():
+	pass
+	#show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	#await $MessageTimer.timeout
+	
+	#$Message.text = "Imminent Meltdown!"
+	#$Message.show()
 	# Make a one-shot timer and wait for it to finish.
-	await get_tree().create_timer(1.0).timeout
-	$StartButton.show()
+	#await get_tree().create_timer(1.0).timeout
+	#$StartButton.show()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,11 +36,12 @@ func _process(delta: float) -> void:
 	pass
 
 func update_score(score):
-	$ScoreLabel.text = str(score)
+	print('updating score')
+	#$ScoreLabel.text = str(score)
 
-func _on_start_button_pressed():
-	$StartButton.hide()
-	start_game.emit()
+#func _on_start_button_pressed():
+	#$StartButtonMargin.hide()
+	#start_game.emit()
 
 func _on_message_timer_timeout():
 	$Message.hide()
