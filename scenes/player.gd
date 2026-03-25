7fe7a3f81fc9d4fe26310b85bf7723992a4fc47c
Jason McCoy <jason@virtualcorners.com>
2026-03-25 11:41:42 -0400

    Make player speed constant

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,9 +5,19 @@ var direction_y := 0
 var speed := 150
 
 func _process(delta: float) -> void:
+	get_input()
+	
+	if direction_x != 0:
+		velocity.x = direction_x * speed
+		velocity.y = 0
+		
+	if direction_y != 0:
+		velocity.y = direction_y * speed
+		velocity.x = 0
+	
+	move_and_slide()
+
+func get_input():
 	direction_x = Input.get_axis("move_left","move_right")
 	direction_y = Input.get_axis("move_up","move_down")
 	
-	velocity.x = direction_x * speed
-	velocity.y = direction_y * speed
-	move_and_slide()
