858d1cf9a5ae94f566a35674876770a950aa7cd7
Jason McCoy <jason@virtualcorners.com>
2026-03-25 18:37:54 -0400

    Set the start positions of player and radioactive worker

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,7 +3,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Player.position = $Player/StartPosition.position
+	$RadioactiveWorker.position = $RadioactiveWorker/StartPosition.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
