d11950a11098585df1ae2aa21122555ea374de0f
Jason McCoy <jason@virtualcorners.com>
2026-03-26 11:28:15 -0400

    Update sin() to work with a float to avoid debugger warning.

--- scenes/conduit.gd
+++ scenes/conduit.gd
@@ -5,7 +5,7 @@ const CONDUIT = preload("res://scenes/conduit.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y += sin(Time.get_ticks_msec() / 200) * 10 * delta
+	position.y += sin(Time.get_ticks_msec() / 200.0) * 10 * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
