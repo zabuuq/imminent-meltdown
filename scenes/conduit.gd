6d393b34e81ec17249d28513b1a719aec4fcab2e
Jason McCoy <jason@virtualcorners.com>
2026-03-25 21:59:49 -0400

    Make is so a piece of conduit can be picked up by a player and mob

--- scenes/conduit.gd
+++ scenes/conduit.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const CONDUIT = preload("res://scenes/conduit.tscn")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -7,5 +9,8 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("grabbed conduit")
-	queue_free()
+	if body.has_node("Holding") and body.get_node("Holding").get_child_count() == 0:
+		var conduit = CONDUIT.instantiate()
+		conduit.position = Vector2.ZERO
+		body.get_node("Holding").add_child(conduit)
+		queue_free()
