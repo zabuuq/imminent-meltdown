ee036f4aff23496df571ed319f4cf925f6c0a764
Jason McCoy <jason@virtualcorners.com>
2026-03-27 17:32:16 -0400

    Update code style

--- scenes/start_game.gd
+++ scenes/start_game.gd
@@ -1,11 +1,11 @@
 extends Control
 
-var start_level: PackedScene = load("res://scenes/main.tscn")
+const MAIN_SCENE = preload('res://scenes/main.tscn')
 
 func _ready() -> void:
-	if has_node("DeathSound"):
+	if has_node('DeathSound'):
 		$DeathSound.play()
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("start_game"):
-		get_tree().change_scene_to_packed(start_level)
+	if Input.is_action_just_pressed('start_game'):
+		get_tree().change_scene_to_packed(MAIN_SCENE)
