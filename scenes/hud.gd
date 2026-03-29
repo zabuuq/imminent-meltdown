3e87d4ce8848565c1077d5f46d287a8458607bf6
Jason McCoy <jason@virtualcorners.com>
2026-03-28 23:39:16 -0400

    Add a score

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,43 +1,46 @@
 extends CanvasLayer
 
-signal game_won
+signal game_won(items_fixed: int)
 
 var damages := 0
 var cooldowns := 0
+var items_fixed := 0
 
 
 func add_damage() -> void:
 	damages += 1
-	$DamagesMargin/VBoxContainer/HBoxContainer/DamagesLabel.text = str(damages)
+	$DamagesContainer/VBoxContainer/HBoxContainer/DamagesLabel.text = str(damages)
 
 
 func remove_damage() -> void:
 	damages -= 1
-	$DamagesMargin/VBoxContainer/HBoxContainer/DamagesLabel.text = str(damages)
+	$DamagesContainer/VBoxContainer/HBoxContainer/DamagesLabel.text = str(damages)
 	if damages == 0 and cooldowns == 0:
-		game_won.emit()
+		game_won.emit(items_fixed)
 
 
 func add_cooldown() -> void:
 	cooldowns += 1
-	$DamagesMargin/VBoxContainer/HBoxContainer2/CooldownLabel.text = str(cooldowns)
+	items_fixed += 1
+	$DamagesContainer/VBoxContainer/HBoxContainer2/CooldownLabel.text = str(cooldowns)
+	$ScoreContainer/HBoxContainer/ScoreLabel.text = str(items_fixed)
 
 
 func remove_cooldown() -> void:
 	cooldowns -= 1
-	$DamagesMargin/VBoxContainer/HBoxContainer2/CooldownLabel.text = str(cooldowns)
+	$DamagesContainer/VBoxContainer/HBoxContainer2/CooldownLabel.text = str(cooldowns)
 	if damages == 0 and cooldowns == 0:
-		game_won.emit()
+		game_won.emit(items_fixed)
 
 
 func set_meltdown_time(t: int) -> void:
-	$TimerMargin/HBoxContainer/MeltdownDisplay.text = "%02d:%02d" % [int(t / 60.0), t % 60]
+	$TimerContainer/HBoxContainer/MeltdownDisplay.text = "%02d:%02d" % [int(t / 60.0), t % 60]
 
 
 func _on_player_update_health(health: float) -> void:
 	var child_num := 0.0
 
-	for child in $LivesMargin.get_children():
+	for child in $LivesContainer.get_children():
 		child_num += 1
 
 		if health == child_num - 0.5:
--- scenes/win_game.gd
+++ scenes/win_game.gd
@@ -1,4 +1,17 @@
 extends Control
+class_name WinGame
+
+static var items_fixed: int = 0
+static var time_left: int = 0
+static var score: int = 0
+
+
+func _ready() -> void:
+	var base := $MessageContainer/VBoxContainer/MarginContainer/VBoxContainer
+	base.get_node("HBoxContainer1/ItemsFixedLabel").text = str(items_fixed)
+	base.get_node("HBoxContainer2/TimeLeftLabel").text = str(time_left)
+	base.get_node("HBoxContainer3/ScoreLabel").text = str(score)
+
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed('start_game'):
